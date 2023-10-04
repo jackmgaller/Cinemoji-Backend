5ad98478381d2957998de8f4a5dc1d0f3640176b
jackmgaller <68564754+jackmgaller@users.noreply.github.com>
2023-10-04 11:51:21 -0500

    Change Movie type

--- main.ts
+++ main.ts
@@ -6,15 +6,9 @@ const cors = require("cors");
 
 type Movie = {
   "posterLink": string;
-  "rank": string;
-  "trend": string;
   "title": string;
   "year": string;
-  "genre": string;
-  "votes": string;
-  "rating": string;
   "emojiSummary": string;
-  "index": string;
 };
 
 const app: Express = express();
